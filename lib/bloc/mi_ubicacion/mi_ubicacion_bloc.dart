c67a43d15db3e3dafa5311608c0d492225b3749e
Efrain May <efrain_may_75@hotmail.com>
2021-02-06 13:32:40 -0600

    segui ubicacion del user

--- lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
+++ lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
@@ -1,14 +1,34 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart' as Geolocator;
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 import 'package:meta/meta.dart';
 
 part 'mi_ubicacion_event.dart';
 part 'mi_ubicacion_state.dart';
 
 class MiUbicacionBloc extends Bloc<MiUbicacionEvent, MiUbicacionState> {
+
   MiUbicacionBloc() : super(MiUbicacionState());
+
+  // ignore: cancel_subscriptions
+  StreamSubscription<Position> _positionSubscripcion;
+
+  void iniciarSeguimiento(){
+
+    Geolocator.Geolocator.getPositionStream(
+      desiredAccuracy: Geolocator.LocationAccuracy.high,
+      distanceFilter: 10
+    ).listen((Geolocator.Position position) {
+      print(position);
+    });
+  }
+
+  void cancelarSeguimiento(){
+    _positionSubscripcion?.cancel();
+  }
 
   @override
   Stream<MiUbicacionState> mapEventToState( MiUbicacionEvent event ) async* {
--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mapas_app/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
+
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+
+  @override
+  void initState() {
+    BlocProvider.of<MiUbicacionBloc>(context).iniciarSeguimiento();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    BlocProvider.of<MiUbicacionBloc>(context).cancelarSeguimiento();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
