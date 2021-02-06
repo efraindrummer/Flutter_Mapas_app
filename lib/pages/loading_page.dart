2a02971cd5dd39ce348436ff3291e52cb88749d3
Efrain May <efrain_may_75@hotmail.com>
2021-02-05 18:30:37 -0600

    deteccion cuando el usuario activo el GPS

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -5,7 +5,34 @@ import 'package:mapas_app/pages/acceso_gps_page.dart';
 import 'package:mapas_app/pages/mapa_page.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends StatefulWidget {
+
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> with WidgetsBindingObserver{
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if(state == AppLifecycleState.resumed){
+      if( await Geolocator.GeolocatorPlatform.instance.isLocationServiceEnabled()){
+        Navigator.pushReplacement(context, navegarMapaFedeIn(context, MapaPage()));
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
